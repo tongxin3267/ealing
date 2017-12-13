0f4022a9f8a1e5b2898d43c1d80e3b8ddc94c406
onep2p <324834500@qq.com>
2017-12-13 10:11:00 +0800

    description
    对商户和普通用户分流验证步骤的逻辑说明

--- application/ealing/controller/AuthApi.php
+++ application/ealing/controller/AuthApi.php
@@ -83,7 +83,7 @@ class AuthApi extends Controller
         
         if(!in_array($this->request->action(), $this->openListAction)) {
             $this->clientInfo = $this->checkAuth();//接口token检查
-            $this->abilitieCheck();//请求接口授权认证
+            $this->abilitieCheck();//请求接口授权认证[如果是外部商户请求则检测商户的指定权限]
         }
     } 
 
@@ -133,12 +133,35 @@ class AuthApi extends Controller
     */
     public function abilitieCheck()
     {
-        $request = $this->request;
-        $module = $request->module();
-        $controller = explode('.', $request->controller())[1];
-        $action = $request->action();
+        $module = $this->request->module();
+        $controller = explode('.', $this->request->controller())[1];
+        $action = $this->request->action();
         
-        if(1==2) return self::returnmsg(401, 'node error!', [], ["Access-Control-Allow-Origin" => $this->restMethodList, 'Content-Type' => $this->restOutputType[$this->type]]);
+        if($this->request->has('signature') && Factory::getInstance(\app\ealing\controller\v1\Token::class)->checkTime()){
+            /**
+             * 外部商户SDK继承请求【当前默认全开权限】
+             * 第一步：检测开通商户的用户【该用户依然可用来登录】
+             * 第二步：以开通用户的节点使用权限验证商户权限【商户用户的权限为独立授权，可单个勾选，不分组】
+             * 第三步：检测请求频率等【限制设置在商户信息中】
+             * 
+             * 商户的开通：
+             * 第一步：注册基础会员
+             * 第二步：开通商户
+             * 第三步：申请或者购买商户权限
+             * @var unknown
+             */
+            $result = true;
+        }else{
+            /**
+             * 内部商户【平台自己商户下的用户】
+             * 第一步：验证用户登录情况
+             * 第二步：验证用户时候有该节点的使用权限
+             * @var unknown
+             */
+            $result = true;
+        }
+
+        if(!$result) return self::returnmsg(401, 'node error!', [], ["Access-Control-Allow-Origin" => $this->restMethodList, 'Content-Type' => $this->restOutputType[$this->type]]);
         
         return true;
     }
