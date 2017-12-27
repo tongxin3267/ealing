44a8dc325fa35a7cca34df5a7ea3f9211374467e
onep2p <324834500@qq.com>
2017-12-27 08:52:25 +0800

    新增新闻模型

---
+++ application/news/model/News.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* 新闻模型
+* @date: 2017年12月27日 上午8:51:32
+* @author: onep2p <324834500@qq.com>
+*/
+namespace app\news\model;
+
+use app\ealing\model\BaseModel;
+
+class News extends BaseModel
+{
+    
+}
