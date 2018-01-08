576ac1d3f614c075a6b0ad262501fd11a07e98ff
onep2p <324834500@qq.com>
2018-01-08 15:30:11 +0800

    add advertising space

--- database/migrations/20180108062117_advertising_space.php
+++ database/migrations/20180108062117_advertising_space.php
@@ -28,6 +28,33 @@ class AdvertisingSpace extends Migrator
      */
     public function change()
     {
-
+        $table = $this->table('advertising_space', array('engine'=>'InnoDB', 'id'=>false, 'primary_key'=>'id'))->setComment('advertising space table');
+        
+        $table
+            ->addColumn(Column::integer('id')->setUnsigned()->setLimit(11)->setComment('advertising space id.'))
+            ->addColumn(Column::string('channel', 50)->setComment('advertising channel.'))
+            ->addColumn(Column::string('space', 191)->setUnique()->setComment('advertising space id.'))
+            ->addColumn(Column::string('alias', 191)->setComment('advertising space alias.'))
+            ->addColumn(Column::string('allow_type', 191)->setComment('advertising space allow type.'))
+            ->addColumn(Column::text('format')->setComment('advertising space data format.'))
+            ->addColumn(Column::text('rule')->setComment('advertising space data rule.'))
+            ->addColumn(Column::text('message')->setComment('advertising space form validation hints.'))
+            ->addColumn(Column::timestamp('created_at')->setNullable()->setDefault(null)->setComment('created time.'))
+            ->addColumn(Column::timestamp('updated_at')->setNullable()->setDefault(null)->setComment('updated time.'))
+            ->create();
+        
+        $table->addIndex('channel')->addIndex('space');
     }
+    
+    /**
+    * Down Method.
+    * @date: 2018年1月8日 下午3:29:39
+    * @author: onep2p <324834500@qq.com>
+    * @param: variable
+    * @return:
+    */
+    public function down()
+    {
+        $this->dropTable('advertising_space');
+    }    
 }
