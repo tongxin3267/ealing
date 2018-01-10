5610570a1b9c49defa7b48d1d54d322504e77055
onep2p <324834500@qq.com>
2018-01-10 11:09:29 +0800

    add wallets migrations

--- database/migrations/20180108062347_wallets.php
+++ database/migrations/20180108062347_wallets.php
@@ -28,6 +28,31 @@ class Wallets extends Migrator
      */
     public function change()
     {
-
+        $table = $this->table('wallets', array('engine'=>'InnoDB', 'id'=>false, 'primary_key'=>'id'))->setComment('wallets table');
+        
+        $table
+            ->addColumn(Column::integer('id')->setUnsigned()->setLimit(11)->setComment('wallets id.'))
+            ->addColumn(Column::integer('user_id')->setUnsigned()->setUnique()->setComment('wallets owner user id.'))
+            ->addColumn(Column::bigInteger('balance')->setUnsigned()->setComment('wallets balance'))
+            ->addColumn(Column::timestamp('created_at')->setNullable()->setDefault(null)->setComment('created time.'))
+            ->addColumn(Column::timestamp('updated_at')->setNullable()->setDefault(null)->setComment('updated time.'))
+            
+            ->addForeignKey('user_id', 'users', 'id', ['delete'=> 'SET_NULL', 'update'=> 'NO_ACTION'])
+            
+            ->addIndex('balance')
+            
+            ->create();
+    }
+    
+    /**
+    * Down Method.
+    * @date: 2018年1月10日 上午10:40:43
+    * @author: onep2p <324834500@qq.com>
+    * @param: variable
+    * @return:
+    */
+    public function down()
+    {
+        $this->dropTable('wallets');
     }
 }
--- database/migrations/20180108062403_wallet_cashes.php
+++ database/migrations/20180108062403_wallet_cashes.php
@@ -28,6 +28,36 @@ class WalletCashes extends Migrator
      */
     public function change()
     {
-
+        $table = $this->table('wallet_cashes', array('engine'=>'InnoDB', 'id'=>false, 'primary_key'=>'id'))->setComment('cashes table');
+        
+        $table
+            ->addColumn(Column::integer('id')->setUnsigned()->setLimit(11)->setComment('cashes id.'))
+            ->addColumn(Column::integer('user_id')->setUnsigned()->setComment('cashes user id.'))
+            ->addColumn(Column::bigInteger('value')->setUnsigned()->setComment('cashes money.'))
+            ->addColumn(Column::string('type', 100)->setComment('cashes type.'))
+            ->addColumn(Column::string('account')->setComment('cashes account.'))
+            ->addColumn(Column::tinyInteger('status')->setUnsigned()->setNullable()->setDefault(0)->setComment('status 0-wait,1-adopt,2-refuse.'))
+            ->addColumn(Column::text('remark')->setNullable()->setDefault(null)->setComment('remark.'))
+            ->addColumn(Column::timestamp('created_at')->setNullable()->setDefault(null)->setComment('created time.'))
+            ->addColumn(Column::timestamp('updated_at')->setNullable()->setDefault(null)->setComment('updated time.'))
+            ->addColumn(Column::timestamp('deleted_at')->setNullable()->setDefault(null)->setComment('deleted time.'))
+            
+            ->addForeignKey('user_id', 'users', 'id', ['delete'=> 'SET_NULL', 'update'=> 'NO_ACTION'])
+            
+            ->addIndex('user_id')
+            
+            ->create();
+    }
+    
+    /**
+    * Down Method.
+    * @date: 2018年1月10日 上午10:54:00
+    * @author: onep2p <324834500@qq.com>
+    * @param: variable
+    * @return:
+    */
+    public function down()
+    {
+        $this->dropTable('wallet_cashes');
     }
 }
