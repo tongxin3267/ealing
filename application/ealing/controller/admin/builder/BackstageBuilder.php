6c854bba3aff0e4fbc73443058edc570b2af66e2
onep2p <324834500@qq.com>
2018-01-19 11:41:59 +0800

    update

--- application/ealing/controller/admin/builder/BackstageBuilder.php
+++ application/ealing/controller/admin/builder/BackstageBuilder.php
@@ -9,25 +9,6 @@ namespace app\ealing\controller\admin\builder;
 use app\ealing\controller\admin\Backstage;
 
 class BackstageBuilder extends Backstage{
-
-//     public function _initializeView()
-//     {
-//         $root = think_get_root();
-//         $viewReplaceStr = [
-//             '__PUBLIC__' => $root.'/',
-//             '__STATIC__' =>$root.'/static',
-//             '__B_IMG__' =>$root.'/backstage/images',
-//             '__B_CSS__' =>$root.'/backstage/css',
-//             '__B_JS__' =>$root.'/backstage/js',
-//             '__ROOT__'=>$root,
-//             '__ZUI__' => $root . '/static/zui',
-//             '__UPLOAD__' => $root,
-
-//         ];
-        
-//         config('view_replace_str', $viewReplaceStr);
-//     }
-
     public function display($templateFile='',$charset='',$contentType='',$content='',$prefix='') {
         //获取模版的名称
         $template = '/admin/builder/'.$templateFile;
